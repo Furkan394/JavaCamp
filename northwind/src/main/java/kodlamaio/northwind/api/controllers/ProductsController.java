d4f8f24a8042ca4d964fdc2669e904dd67d40968
Furkan394 <furkanncakiroglu@gmail.com>
2021-05-22 23:53:57 +0300

    core layer added , lombok refactored

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -4,14 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
 @RestController
-@RequestMapping("/api/products") //istek gelince karşıla
+@RequestMapping("/api/products") 
 public class ProductsController {
 		
 		private ProductService productService;
@@ -23,7 +27,11 @@ public class ProductsController {
 		}
 
 		@GetMapping("/getall")
-		public List<Product> getAll() { //methodun desteğini controller sayesinde veriyoruz
+		public DataResult<List<Product>> getAll() { 
 			return this.productService.getAll();
 		}
+		
+		@PostMapping("/add")
+		public Result add(@RequestBody Product product) {
+			return this.productService.add(product);		}
 }
