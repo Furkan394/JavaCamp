fc94add1e6a8da7926467e4a72911e037d29315f
Furkan394 <furkanncakiroglu@gmail.com>
2021-05-31 00:31:25 +0300

    30.05 Camp Day 9

--- northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 public interface ProductService {
 	DataResult<List<Product>> getAll();
@@ -29,5 +30,6 @@ public interface ProductService {
 	
 	DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
 	
+	DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails();
 	
 }
